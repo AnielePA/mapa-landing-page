656f4533049785bae041174dd8b133b6775c1a93
aniele <ardissonaniele99@gmail.com>
2025-06-26 11:31:04 -0400

    feat(footer) implementando footer responsivo

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+//função para abrir o menu dropdown e fechar:
+
 let menuList = document.getElementById("menuList")
 menuList.style.maxHeight = "0px";
 
@@ -12,51 +15,57 @@ function toggleMenu() {
 
 
 
-// 1. Encontrar todas as nossas "curiosidades" (que agora são articles).
-// O detetive agora procura por '.curiosity-item'
+//encontrar todas as curiosidades e os botões de próximo e anterior:
 const curiosidades = document.querySelectorAll('.curiosity-item');
-
-// 2. Encontrar o botão "Próximo" e o botão "Anterior".
 const botaoProximo = document.getElementById('botaoProximo');
 const botaoAnterior = document.getElementById('botaoAnterior');
 
-// 3. Nosso contador para saber qual curiosidade está aparecendo.
+//contador para ver qual curiosidade está sendo mostrada no momento:
 let indiceAtual = 0;
 
-// 4. A função para ir para a PRÓXIMA curiosidade.
-function mostrarProximaCuriosidade() {
-    // Esconde a curiosidade atual.
-    curiosidades[indiceAtual].classList.remove('active');
 
-    // Avança para o próximo número.
+//função que passa para próxima curiosidade:
+function mostrarProximaCuriosidade() {
+    curiosidades[indiceAtual].classList.remove('active');
     indiceAtual++;
 
-    // Se chegou no final, volta para a primeira.
     if (indiceAtual >= curiosidades.length) {
         indiceAtual = 0;
     }
 
-    // Mostra a nova curiosidade.
     curiosidades[indiceAtual].classList.add('active');
 }
 
-// 5. A função para ir para a curiosidade ANTERIOR.
+//função que passa para a curiosidade anterior:
 function mostrarCuriosidadeAnterior() {
-    // Esconde a curiosidade atual.
     curiosidades[indiceAtual].classList.remove('active');
 
-    // Volta para o número anterior.
     indiceAtual--;
 
-    // Se tentou ir antes da primeira, volta para a última.
     if (indiceAtual < 0) {
         indiceAtual = curiosidades.length - 1;
     }
 
-    // Mostra a nova curiosidade.
     curiosidades[indiceAtual].classList.add('active');
 }
 
-// 6. Dizer aos botões para chamar suas funções quando forem clicados.
+//para os botões chamarem suas funções quando forem clicados:
 botaoProximo.addEventListener('click', mostrarProximaCuriosidade);
 botaoAnterior.addEventListener('click', mostrarCuriosidadeAnterior);
+
+
+
+//função para saber o ano atual e atualizar o texto interno do <span> (encontrado pelo id) no footer:
+
+function atualAno() {
+    const anoAtual = new Date().getFullYear();
+    return anoAtual;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const elementoAno = document.getElementById('ano-copyright');
+
+    if (elementoAno) {
+        elementoAno.textContent = atualAno();
+    }
+});
