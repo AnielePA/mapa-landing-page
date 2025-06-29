31e51f73bf222aa56b074a283ccdae4b8bc80ee4
aniele <ardissonaniele99@gmail.com>
2025-06-29 14:16:32 -0400

    fix(menu): corrigido erro na função do dropdown

--- src/js/app.js
+++ src/js/app.js
@@ -5,10 +5,10 @@
 
 //função para abrir o menu dropdown e fechar:
 
-function toggleMenu() {
-    let menuList = document.getElementById("menuList")
-    menuList.style.maxHeight = "0px";
 
+let menuList = document.getElementById("menuList")
+menuList.style.maxHeight = "0px";
+function toggleMenu() {
 
     if (menuList.style.maxHeight == "0px") {
         menuList.style.maxHeight = "300px";
